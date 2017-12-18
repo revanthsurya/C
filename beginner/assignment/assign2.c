19181c2733de8b56bce2a9f9c51f7ba2d2eb3ea7
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2017-12-18 09:24:11 +0000

    Update assign2.c

--- beginner/assignment/assign2.c
+++ beginner/assignment/assign2.c
@@ -6,6 +6,6 @@ main() {
 	int c=18426;
 	float d=44.83;
 	int e=463;
-            printf("%s \n %s \n %d \n %f \n %d",a,b,c,d,e);
+            printf("%s \n %s \n %d \n %.2f \n %d",a,b,c,d,e);
 	return 0;
 }
