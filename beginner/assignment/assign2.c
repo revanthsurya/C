75c1b0615a110b6165dc843ab3f326feae6cc68c
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2017-12-18 09:09:44 +0000

    Create assign2.c

---
+++ beginner/assignment/assign2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main() {
+	char a[]="Sachin Ramesh Tendulkar";
+	char b[]="Little Master";
+	int c=18426;
+	float d=44.83;
+	int e=463;
+            printf("%s \n %s \n %d \n %f \n %d",a,b,c,d,e);
+	return 0;
+}
