7a6d3f435c23207579ceb435276f6a27a4962e42
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2017-12-28 16:35:47 +0530

    Create monthly_pay.py

---
+++ python/hand_practice/monthly_pay.py
@@ -0,0 +1,5 @@
+hours_worked=int(input())
+pay_rate=int(input())
+weeks_in_month=int(input())
+monthly_pay=hours_worked*pay_rate*weeks_in_month
+print("The monthly pay of the employee is",monthly_pay)
