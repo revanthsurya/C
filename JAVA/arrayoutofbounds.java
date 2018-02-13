92a7a15077efae74907f9d8ebc1e8aa596ebe97d
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-02-13 10:33:24 -0500

    Update arrayoutofbounds.java

--- JAVA/arrayoutofbounds.java
+++ JAVA/arrayoutofbounds.java
@@ -1,1 +1,26 @@
-bjy
+package coder;
+import java.util.Scanner;
+public class Arrayoutofbounds {
+public static void main(String args[])
+{
+	int n;
+	Scanner s=new Scanner(System.in);
+	n=s.nextInt();
+	int []arr=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		arr[i]=s.nextInt();
+	}
+	try
+	{
+		int op=arr[6];
+		System.out.println(op);
+		
+	}
+	catch(ArrayIndexOutOfBoundsException e)
+	{
+		System.out.println("array size exceeded" +e);
+	}
+			
+}
+
