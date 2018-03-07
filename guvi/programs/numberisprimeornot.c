7023bb6d796897f52e407222a214b8ab5d20f399
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-03-07 10:32:24 -0500

    Update numberisprimeornot.c

--- guvi/programs/numberisprimeornot.c
+++ guvi/programs/numberisprimeornot.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
- int main(void) {
-int i,n,j=0;
-printf("\n Enter the number:");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+int main()
 {
-	if(n%i==0)
-	{
-		j++;
-	}
-}
-if(j==2)
-{
-	printf("\nprime");
- 
-}
-else
-{
-	printf("\nNot prime");
-}
-	return 0;
+    int n, i, flag = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=2; i<=n/2; ++i)
+    {
+        // condition for nonprime number
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==0)
+        printf("%d is a prime number.",n);
+    else
+        printf("%d is not a prime number.",n);
+    
+    return 0;
 }
