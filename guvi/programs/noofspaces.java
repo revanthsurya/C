4d5b93603a13a87e6cc128b56b2d5aa6fba21f24
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-03-07 11:08:15 -0500

    Create noofspaces.java

---
+++ guvi/programs/noofspaces.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int len=str.split(" ").length;
+	            System.out.println(len-1);
+	}
+}
