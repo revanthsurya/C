33e8b0aa9d8b35ce0d6c344dfdc439ee32587b04
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-03-07 10:42:46 -0500

    Update displayevennumbers.c

--- guvi/programs/displayevennumbers.c
+++ guvi/programs/displayevennumbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void) {
-	int a,b,i;
+	int a,b;
  printf("\n Enter the range:");
  scanf("%d%d",&a,&b);
- for(i=a+1;i<b;i++)
+ for(int i=a;i<b;i++)
  {
  	if(i%2==0)
  	printf("%d\n",i);
