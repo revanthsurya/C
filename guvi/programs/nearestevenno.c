3eee07d4ac6d60c287cb536a0d397e3527806ea7
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-03-08 08:57:48 +0530

    Create nearestevenno.c

---
+++ guvi/programs/nearestevenno.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+int n,digit,sum=0;
+scanf("%d",&n);
+             if(n%2==0)
+             {
+             	printf("%d",n);
+             }
+             else
+             {
+             	printf("\n %d",n-1);
+             }
+	
+return 0;
+}
