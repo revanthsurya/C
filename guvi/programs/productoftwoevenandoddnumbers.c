af16b70fd13e08f4944a31c8e57347d742be4924
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-03-08 08:59:00 +0530

    Create productoftwoevenandoddnumbers.c

---
+++ guvi/programs/productoftwoevenandoddnumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+	int n,m,p;
+	scanf("%d %d",&n,&m);
+	p=n*m;
+             if(p%2==0)
+             {
+             	printf("even");
+             }
+             else
+             {
+             	printf("Odd");
+             }
+	
+	return 0;
+}
