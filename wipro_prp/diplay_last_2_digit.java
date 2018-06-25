155a8e9a759dcc99a1bd29aa4c329b77731173f9
revanthsurya <34641063+revanthsurya@users.noreply.github.com>
2018-06-25 09:55:45 +0530

    Create diplay_last_2_digit.java

---
+++ wipro_prp/diplay_last_2_digit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    int i,n;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    i=n%100;
+    System.out.println(i);
+  }
+}
